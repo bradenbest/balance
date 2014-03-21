7b612afd2528c1282eeb0c1a00dff773b26c77e1
bradenbest <bradentbest@gmail.com>
2014-03-21 03:13:13 -0600

    added debug log to startup, updated version

--- src/main.c
+++ src/main.c
@@ -16,13 +16,20 @@ int main(int argc, char **argv){
 
   set_version(&v);
 
+  if(config.interface.DEBUG_MODE)    printf("DEBUG :: Checking for arguments.\n");
   if(argc > 1){ // Argument is passed
     strcpy(fname,argv[1]);
+    if(config.interface.DEBUG_MODE)    printf("DEBUG :: Argument found\nDEBUG :: Trying argument \"%s\"\n", fname);
   }else{
+    if(config.interface.DEBUG_MODE)    printf("DEBUG :: No arguments found\nDEBUG :: Checking for config.\n");
     if(config.data.FILENAME[0] != 0){ // File specified in config.c
       strcpy(fname,config.data.FILENAME);
+      if(config.interface.DEBUG_MODE)    printf("DEBUG :: Config found\nDEBUG :: Trying config.data.FILENAME (\"%s\").\n", fname);
     }else{ // Neither an argument is passed, nor file specified. Default to checking $HOME
       get_home(fname);
+      if(config.interface.DEBUG_MODE){
+        printf("DEBUG :: No File Specified in config.\nDEBUG :: Defaulting to HOME environment variable (\"%s\"). If this fails, the program will segfault.\n", fname);
+      }
     }
   }
   
